2eb6a92cfb5a8fbbccc897cb0c1f196e9870f101
banjohirvi <ollilari@gmail.com>
2016-01-20 00:24:50 +0200

    Tehtava 12

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,28 @@
 # kirjoita koodi tänne
+def luokittelu(a)
+  nega = []
+  posi = []
+  pari = []
+  pariton = []
+  sum = 0
+  a.each do |alkio|
+    sum += alkio
+    if alkio % 2 == 0
+      pari << alkio
+    else
+      pariton << alkio
+    end
+    if alkio < 0
+      nega << alkio
+    else
+      posi << alkio
+    end
+  end
+  nega = nega.sort
+  posi = posi.sort
+  pari = pari.sort
+  pariton = pariton.sort
+
+  b = {negatiivinen: nega, positiivinen: posi, parillinen: pari, pariton: pariton, summa: sum}
+
+end
