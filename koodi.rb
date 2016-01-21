5d70f04636ecb4ea2176dbbc8dd315d92ce905b9
Tontsu <Tontsu@Tontsus-MacBook-Air.local>
2016-01-21 13:07:21 +0200

    doned

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
 # kirjoita koodi tänne
+def luokittelu (taulukko)
+  hashi = {}
+  hashi[:negatiivinen] = taulukko.reject {|i| i > 0}.sort
+  hashi[:positiivinen] = taulukko.reject {|i| i <= 0}.sort
+  hashi[:parillinen] = taulukko.reject {|i| i.odd?}.sort
+  hashi[:pariton] = taulukko.reject {|i| not i.odd?}.sort
+  hashi[:summa] = taulukko.inject(0) {|tulos, x| tulos + x}
+  hashi
+end
