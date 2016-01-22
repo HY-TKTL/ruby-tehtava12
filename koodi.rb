dfaf23353d05d8ea22cc1217cfdd6ed55824da7a
Owlaukka <olli.laukkanen3@gmail.com>
2016-01-22 22:00:07 +0200

    luokittelu

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
-# kirjoita koodi tänne
+def luokittelu (taulukko)
+  h = {}
+  h[:negatiivinen] = taulukko.select { |alkio| alkio < 0 }.sort
+  h[:positiivinen] = taulukko.select { |alkio| alkio >= 0 }.sort
+  h[:parillinen] = taulukko.select { |alkio| alkio.even? }.sort
+  h[:pariton] = taulukko.select { |alkio| alkio.odd? }.sort
+
+  if (taulukko.length == 0) then
+    h[:summa] = 0
+  else
+    h[:summa] = taulukko.inject { |tulos, x| tulos + x }
+  end
+  h
+end
