c8b389841363aa8da1d5bde76fe9c2d5d0ce43a3
henri <pulperi@gmail.com>
2016-01-23 21:01:27 +0200

    palautus

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
 # kirjoita koodi tänne
+def luokittelu(taulukko)
+  h = {}
+  h[:negatiivinen] = taulukko.reject { |alkio| alkio >= 0 }.sort
+  h[:positiivinen] = taulukko.reject { |alkio| alkio <= 0 }.sort
+  h[:parillinen] = taulukko.reject { |alkio| alkio % 2 != 0 }.sort
+  h[:pariton] = taulukko.reject { |alkio| alkio % 2 == 0 }.sort
+  h[:summa] = taulukko.empty? ? 0 : taulukko.inject(:+)
+  h
+end
