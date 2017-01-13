35817a5e556c5aa2a031565c462e574266b5aecb
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-01-13 15:31:45 +0200

    opin käyttämään if lausetta sekä inject metodia. kertasin select, sort metodeja.

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,13 @@
-# kirjoita koodi tänne
+def luokittelu(taulukko)
+    hash = {}
+    hash[:negatiivinen] = taulukko.select{|a| a < 0 }.sort
+    hash[:positiivinen] = taulukko.select{|a| a > 0 }.sort
+    hash[:parillinen] = taulukko.select{|a| a.even? }.sort
+    hash[:pariton] = taulukko.select{|a| !a.even? }.sort
+    hash[:summa] = taulukko.inject(:+)
+    if taulukko.empty?
+       hash[:summa] = 0
+    end
+   return hash
+end
+
