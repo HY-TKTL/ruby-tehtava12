0bbdef0e97d2257cfaf2de3ea33a3f37842d94f6
Mikko Nikkilä <mikko.nikkila@iki.fi>
2022-09-17 22:10:38 +0300

    Tehtävä 12

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,15 @@
 # kirjoita koodi tänne
+def luokittelu(taulukko)
+    positiivinen = []
+    negatiivinen = []
+    parillinen = []
+    pariton = []
+    taulukko.each do |x|
+        if x > 0 then positiivinen.push(x) end
+        if x < 0 then negatiivinen.push(x) end
+        if x % 2 == 0 then parillinen.push(x) else pariton.push(x) end
+    end
+    summa = taulukko.inject(:+)
+    if (summa == nil) then summa = 0 end
+    {negatiivinen: negatiivinen.sort, positiivinen: positiivinen.sort, parillinen: parillinen.sort, pariton: pariton.sort, summa: summa}
+end
