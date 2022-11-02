2139e14322e9ea08f5ebb70cf444d700def8572c
Hannu Kulmala <hanski22@pm.me>
2022-11-02 13:09:25 +0200

    Tehtava12

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,12 @@
-# kirjoita koodi tänne
+def luokittelu(taulu)
+    taulu = taulu.sort()
+    hash = { negatiivinen: [], positiivinen: [], parillinen: [], pariton: [], summa: 0 }
+    taulu.each do |item|
+        hash[:positiivinen] << item if item > 0
+        hash[:negatiivinen] << item if item < 0
+        hash[:parillinen] << item if item % 2 == 0
+        hash[:pariton] << item if item % 2 == 1
+    end 
+    hash[:summa] = taulu.inject(:+) if not taulu.empty?
+    return hash
+end
