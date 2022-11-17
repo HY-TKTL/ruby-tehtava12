2abeae9f96cb93ec9ebdea9458a3868a61a80828
Jenni Nylund <jenni.esprit@gmail.com>
2022-11-17 08:36:10 +0200

    teht_12

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,13 @@
 # kirjoita koodi tänne
+def luokittelu(taulukko)
+    taulukko.sort!
+    hash = Hash.new(0)
+    hash[:negatiivinen] = taulukko.select{ |i| i < 0 }
+    hash[:positiivinen] = taulukko.select{ |i| i > 0 }
+    hash[:parillinen] = taulukko.select{ |i| i.even? }
+    hash[:pariton] = taulukko.select{ |i| i.odd? }
+    hash[:summa] = taulukko.sum 
+    return hash
+end
+
+luokittelu([1,2,-5,6,-6,11])
